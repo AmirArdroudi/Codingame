ed267af613f8850dd873ced31f3c398558ecda79
AmirArdroudi <a.ardroudi@gmail.com>
2020-12-29 16:30:36 +0330

    CodeRoyal cpp rank wood 1 - 7th

--- codeRoyal.cpp
+++ codeRoyal.cpp
@@ -7,8 +7,52 @@
 
 using namespace std;
 
-#define KNIGHT "KNIGHT"
-#define ARCHER "ARCHER"
+
+class Site
+{
+public:
+    // id of the site
+    int id;
+    // coordination of site
+    int x, y;
+    // redius of site
+    int radius;
+    // distance from queen: should be updated each turn
+    float distance;
+
+    // -1:No structure  0:Goldmine  1:Tower  2:Barracks
+    int structure_type;
+    enum e_StructureTypes { None = -1, Mine, Tower, Barracks };
+
+    // -1:No structure   0:Friendly   1:Enemy
+    int owner;
+    /*
+    When no structure: -1
+    When goldmine: the income rate ranging from 1 to 5 (or -1 if enemy)
+    When tower: the remaining HP
+    When barracks, the number of turns before a new set of creeps can be trained (if 0, then training may be started this turn)
+    */
+    int param1;
+    /*
+    When no structure: -1
+    When goldmine: -1
+    When tower: the attack radius measured from its center
+    When barracks: the creep type: 0 for KNIGHT, 1 for ARCHER, 2 for GIANT
+    */
+    int param2;
+    
+
+    Site(){}
+
+    Site(int _id, int _x, int _y, int _radius): id(_id), x(_x), y(_y), radius(_radius)
+    {} 
+    
+    float CalDistance(int _xq, int _yq)
+    {
+        float distance = sqrt(pow((_xq - x), 2) + pow((_yq - y), 2));
+        return distance;
+    }
+};
 
 class Unit
 {
@@ -28,43 +72,10 @@ public:
     // Dummy constructor
     Unit() {}
 
-    Unit(int _x, int _y, int _owner, int _unitType, int _health) :
-            x(_x), y(_y), owner(_owner), unitType(_unitType), health(_health)
+    Unit(int _x, int _y, int _owner, int _unitType, int _health) 
+            : x(_x), y(_y), owner(_owner), unitType(_unitType), health(_health)
     {}
 
-};
-
-class Site
-{
-public:
-    // id of the site
-    int id;
-    // coordination of site
-    int x, y;
-    // redius of site
-    int radius;
-    // distance from queen: should be updated each turn
-    float distance;
-
-    // -1:No structure   2:Barracks
-    int structure_type;
-
-    // -1:No structure   0:Friendly   1:Enemy
-    int owner;
-    // -1 = No structure, 0= train this turn, ow= number of turns befor a new set of creeps can be trained 
-    int TrainTurns;
-
-    Site(){}
-
-    Site(int _id, int _x, int _y, int _radius): id(_id), x(_x), y(_y), radius(_radius)
-    {
-    } 
-    
-    float CalDistance(int _xq, int _yq)
-    {
-        float distance = sqrt(pow((_xq - x), 2) + pow((_yq - y), 2));
-        return distance;
-    }
 };
 
 class Commands
@@ -81,9 +92,16 @@ public:
         cout << "MOVE " << _x <<" "<< _y <<endl;
     }
 
-    static int Build(string type, int _siteId)
+    // TODO: Use enum for different type of buildings
+    static int Build(string _type, int _siteId)
     {
-        cout<< "BUILD "<< _siteId <<" BARRACKS-"<< type << endl;
+        if(_type == "KNIGHT" || _type == "ARCHER")
+            cout<< "BUILD "<< _siteId <<" BARRACKS-"<< _type << endl;
+        else if(_type == "TOWER")
+            cout<< "BUILD "<< _siteId << " TOWER" << endl;
+        else
+            cout<< "BUILD "<< _siteId <<" MINE"<< endl;
+
         return _siteId;
     }
 
@@ -99,34 +117,35 @@ public:
         for(auto site : v_Barracks)
         {
             // TODO: Remove destroyed barracks
-            if(site.owner != 0)
-            {
-                v_Barracks.pop_back();
-            }
-            if(_gold == 80)
-            {
-                if(site.structure_type == 2)
-                os << " " << site.id;
-                string ids(os.str());
-                cout << "TRAIN" << ids <<endl;        
-                return;
-            }
-            if(site.owner != -1)
-                os <<" " << site.id;
+            os << " " << site.id;
         }
         string ids(os.str());
         cout << "TRAIN" << ids <<endl;
     }
 
+    static int FindNearestSite(vector<Site> &_sites)
+    {
+        int nearestId = 0;
+        for(Site site : _sites)
+        {
+            if(site.distance < _sites[nearestId].distance && site.structure_type == -1 && site.owner != 0)
+            {
+                nearestId = site.id;
+            }
+        }
+
+        return nearestId;
+    }
 };
 
 int main()
 {
     int numSites;
     cin >> numSites;
-    Site* sites = new Site[numSites];
-    vector<Site> barracks;
-    barracks.clear();
+    vector<Site> v_sites;
+    vector<Site> v_mySites;
+    v_mySites.clear();
+    v_sites.clear();
     vector<Site> enemyBarracks;
 
     cin.ignore();
@@ -139,9 +158,7 @@ int main()
         int radius;
         cin >> siteId >> x >> y >> radius; cin.ignore();
 
-        sites[i] = Site(siteId, x, y, radius);
-        // cerr <<"id: " <<sites[i].id <<"   "<<sites[i].x <<","<< sites[i].y <<endl;
-
+        v_sites.push_back({siteId, x, y, radius});
     }
 
     // game loop
@@ -151,25 +168,28 @@ int main()
         int touchedSite; // -1 if none
         int nearestSite_id;
         cin >> gold >> touchedSite; cin.ignore();
-
+        
+        // -------------------------------- Update Sites --------------------------------
         for (int i = 0; i < numSites; i++) 
         {
             int siteId;
-            int ignore1; // used in future leagues
-            int ignore2; // used in future leagues
+            int gold; // used in future leagues
+            int maxMineSize; // used in future leagues
             int structureType; // -1 = No structure, 2 = Barracks
             int owner; // -1 = No structure, 0 = Friendly, 1 = Enemy
-            int param1; // -1 = No structure, 0= train this turn, ow= number of turns befor a new set of creeps can be trained 
-            int param2;
 
-            cin >> siteId >> ignore1 >> ignore2 >> structureType >> owner >> param1 >> param2; cin.ignore();
+            int param1;
+      
+            int param2; 
 
-            if(sites[i].id == siteId)
+            cin >> siteId >> gold >> maxMineSize >> structureType >> owner >> param1 >> param2; cin.ignore();
+
+            if(v_sites[i].id == siteId)
             {
-                sites[i].owner = owner;
-                sites[i].structure_type = structureType;
-                sites[i].TrainTurns = param1;
-                // cerr << sites[i] <<endl;
+                v_sites[i].owner = owner;
+                v_sites[i].structure_type = structureType;
+                v_sites[i].param1 = param1;
+                v_sites[i].param2 = param2;
             }
            // TODO: get enemy barracks
 
@@ -178,9 +198,10 @@ int main()
         // -------------------------------- Update Units --------------------------------
         int numUnits;
         cin >> numUnits; cin.ignore();
-        Unit* myUnits = new Unit[numUnits]; 
-        Unit myQueen;
-        Unit enemyQueen;
+
+        Unit* myQueen = nullptr;
+        Unit* enemyQueen = nullptr;
+        
         for (int i = 0; i < numUnits; i++) 
         {
             int x;
@@ -189,65 +210,64 @@ int main()
             int unitType; // -1 = QUEEN, 0 = KNIGHT, 1 = ARCHER
             int health;
             cin >> x >> y >> owner >> unitType >> health; cin.ignore();
-            // our units
-            if(owner == 0)
-            {
-                myUnits[i] = Unit(x, y, owner, unitType, health);
-                if(unitType == -1)
-                {
-                    myQueen = myUnits[i];
-                }
-            }
-            else
-            {
-                // TODO: find enemy queen
-            }
 
+            // Get Queens
+            if(unitType == -1)
+            {
+                if(owner == 0)
+                    myQueen = new Unit(x, y, owner, unitType, health);
+                else
+                    enemyQueen = new Unit(x, y, owner, unitType, health);
+            }   
         }
         // --------------------------------------------------------------------------------
+
 
         // ---------------- Calculate distance of each site from the queen ----------------
         for(int i = 0; i < numSites; i++)
         {
-            sites[i].distance = sites[i].CalDistance(myQueen.x, myQueen.y);
+            v_sites[i].distance = v_sites[i].CalDistance(myQueen->x, myQueen->y);
         }
         // --------------------------------------------------------------------------------
-        // find nearest not built site
-        nearestSite_id = 0;
-        for(int i = 1; i < numSites; i++)
-        {
-            if(sites[i].distance < sites[nearestSite_id].distance && sites[i].structure_type == -1 && sites[i].owner != 0)
-            {
-                nearestSite_id = i;
-            }
-        }
-        cerr << "nearest site id: " << nearestSite_id << endl; 
+        
+        nearestSite_id = Commands::FindNearestSite(v_sites);
+        
 
-        if(barracks.size() < 2)
+        if(v_mySites.size() < 5)
         {
-
-            if(touchedSite == nearestSite_id )
+            if(touchedSite == nearestSite_id)
             {
-                if(barracks.size() < 1)
-                    Commands::Build(ARCHER, nearestSite_id);
+                if(v_mySites.size() < 2)
+                    Commands::Build("KNIGHT", nearestSite_id);
+                else if(v_mySites.size() == 3)
+                    Commands::Build("TOWER", nearestSite_id);
                 else
-                    Commands::Build(KNIGHT, nearestSite_id);
-
-                sites[nearestSite_id].owner = 0;    
-                barracks.push_back(sites[nearestSite_id]);
+                    Commands::Build("MINE", nearestSite_id);
+               
             }
             // if the site hasn't built yet
-            else if(sites[nearestSite_id].structure_type != 2 || sites[nearestSite_id].owner != 0)
-            {
-                Commands::MoveTo(sites[nearestSite_id]);
-            }    
+            else if(v_sites[nearestSite_id].structure_type != 2 || v_sites[nearestSite_id].owner != 0)
+                Commands::MoveTo(v_sites[nearestSite_id]);
         }
         else
         {
             // TODO: hide behind your barracks
             Commands::Wait();
-        }       
+        }
 
-        Commands::Train(barracks, gold);
+        for(int i = 0; i < v_mySites.size(); i++)
+        {
+        if(v_sites[touchedSite].owner == 0 && (v_sites[touchedSite].id != v_mySites[i].id))
+        {
+            v_mySites.push_back(v_sites[touchedSite]);
+            cerr <<"my sites: "<< v_mySites.size() << "|| " << endl;
+        }
+        }
+        
+        // TODO: update barracks list
+        vector<Site> v_barracks;
+        
+   
+        Commands::Train(v_barracks, gold);
     }
 }
